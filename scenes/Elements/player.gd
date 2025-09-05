ecf9d5d495279ed087eb45967eedadceae99918e
unknown <lides.sam@gmail.com>
2025-09-04 21:52:42 -0300

    new feature: added gravity fall speed

--- scenes/Elements/player.gd
+++ scenes/Elements/player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody3D
 
 var speed: float = 10.0
+var gravity: float = 9.8 *50
 var move_velocity: Vector3 = Vector3.ZERO
+
+@onready var fsm=$Fsm
 
 func _physics_process(delta: float) -> void:
 	
@@ -16,9 +19,24 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("ui_down"):
 		dir += transform.basis.z  # Move forward relative to facing direction
 	# Normalize to prevent faster diagonal movement
+	
 	if dir != Vector3.ZERO:
 		dir = dir.normalized()
 
+		
 	move_velocity = dir * speed
 	velocity = move_velocity
+	if not grounded():
+		velocity.y -= gravity * delta
+	else:
+		if velocity.y < 0:
+			velocity.y = 0
 	move_and_slide()
+	
+func grounded():
+	var ray= $groundDetector
+	return ray.is_colliding()
+	
+func is_on_front():
+	var ray= $onfront
+	return ray.is_colliding()
--- scenes/world/entrada_ilumiada.gd
+++ scenes/world/entrada_ilumiada.gd
@@ -3,5 +3,6 @@ extends Node3D
 func _ready() -> void:
 	if !Global.player:
 		var player =load("res://scenes/Elements/Illusia.tscn").instantiate()
+		player.global_position= $droppoint.global_position
 		$actors.add_child(player)
 		Global.player=player
